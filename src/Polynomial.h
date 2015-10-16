06a81bde4c3925ca775ac08981ae42f32bc60bd6
Daria Kolodzey <kolodaria@google.com>
2015-10-16 22:27:43 +0300

    Create class Polynomial

---
+++ src/Polynomial.h
@@ -0,0 +1,14 @@
+//
+// Created by Дарья Колодзей on 16.10.15.
+//
+
+#ifndef EQUATIONS_POLYNOMIAL_H
+#define EQUATIONS_POLYNOMIAL_H
+
+
+class Polynomial {
+
+};
+
+
+#endif //EQUATIONS_POLYNOMIAL_H
