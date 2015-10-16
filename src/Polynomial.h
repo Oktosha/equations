a6eca37dadc5087cf7f53d8b95250ad68dfbaf77
Daria Kolodzey <kolodaria@google.com>
2015-10-16 23:55:54 +0300

    Add constructors from Variable to Polynomial

--- src/Polynomial.h
+++ src/Polynomial.h
@@ -10,8 +10,13 @@
 
 template <class Coefficient> class Polynomial {
     Polynomial() : _summands() {}
+
+    Polynomial(Variable x) : _summands({Monomial(x), Coefficient(1)}) {}
     Polynomial(Monomial monomial) : _summands({{monomial, Coefficient(1)}}) {}
+
     Polynomial(Monomial monomial, Coefficient a) : _summands({{monomial, a}}) {}
+    Polynomial(Variable x, Coefficient a) : _summands({Monomial(x), a}) {}
+
     Polynomial(const Polynomial& polynomial) : _summands(polynomial._summands) {}
     Polynomial& operator = (Polynomial polynomial) {
         polynomial.swap(*this);
@@ -20,6 +25,7 @@ template <class Coefficient> class Polynomial {
     void swap(Polynomial& polynomial) {
         std::swap(_summands, polynomial._summands);
     }
+
   private:
     std::map<Monomial, Coefficient>_summands;
 };
