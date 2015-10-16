868be1b6c968b421dd203d686256b4a3ba18c9bf
Daria Kolodzey <kolodaria@google.com>
2015-10-16 23:52:47 +0300

    Add constructors to Polynomial

--- src/Polynomial.h
+++ src/Polynomial.h
@@ -6,8 +6,22 @@
 #define EQUATIONS_POLYNOMIAL_H
 
 
-class Polynomial {
+#include "Monomial.h"
 
+template <class Coefficient> class Polynomial {
+    Polynomial() : _summands() {}
+    Polynomial(Monomial monomial) : _summands({{monomial, Coefficient(1)}}) {}
+    Polynomial(Monomial monomial, Coefficient a) : _summands({{monomial, a}}) {}
+    Polynomial(const Polynomial& polynomial) : _summands(polynomial._summands) {}
+    Polynomial& operator = (Polynomial polynomial) {
+        polynomial.swap(*this);
+        return *this;
+    }
+    void swap(Polynomial& polynomial) {
+        std::swap(_summands, polynomial._summands);
+    }
+  private:
+    std::map<Monomial, Coefficient>_summands;
 };
 
 
