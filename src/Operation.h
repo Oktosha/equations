92a69380e64408959fb280a0c1b3087c1f0d8d71
Daria Kolodzey <kolodaria@google.com>
2015-10-17 15:14:38 +0300

    Add const modifier to Operation virtual methods

--- src/Operation.h
+++ src/Operation.h
@@ -6,14 +6,15 @@
 #define EQUATIONS_OPERATION_H
 
 #include <vector>
+
 template <class Value>
 class Operation {
   public:
     Operation() = delete;
     Operation(const Operation&) = delete;
     Operation& operator = (const Operation&) = delete;
-    virtual size_t getArity() = 0;
-    virtual Value getValue(const std::vector<Value>& args) = 0;
+    virtual size_t getArity() const = 0;
+    virtual Value getValue(const std::vector<Value>& args)const = 0;
     virtual ~Operation() {}
 };
 
